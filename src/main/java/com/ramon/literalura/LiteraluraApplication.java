8567654142348a67705159258d4aedb12e13ae94
Ramon Moreira da Silva <ramon.files.32@gmail.com>
2026-03-10 17:01:23 -0300

    feat: adiciona jackson e cria conversor generico de dados json

---
+++ src/main/java/com/ramon/literalura/LiteraluraApplication.java
@@ -0,0 +1,21 @@
+package com.ramon.literalura;
+
+import com.ramon.literalura.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteraluraApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        var consumoApi = new ConsumoApi();
+        var json = consumoApi.obterDados("https://gutendex.com/books/?search=machado+de+assis");
+        System.out.println(json);
+    }
+}
---
+++ src/main/java/com/ramon/literalura/service/ConverteDados.java
@@ -0,0 +1,17 @@
+package com.ramon.literalura.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements  IConverteDados{
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> tClass) {
+        try {
+            return  mapper.readValue(json, tClass);
+        }catch (JsonProcessingException e){
+            throw new RuntimeException("Erro ao converter JSON: " + e.getMessage());
+        }
+    }
+}
